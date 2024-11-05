d6f8c3e0ba62c6dec48d543fe2bedc07bcd5548f
Ranny Gabriel <rannygabrizao12@gmail.com>
2024-11-05 09:31:14 -0400

    Delete TrabalhoPOO1/TrabalhoPOO1 directory
    apagando porque deu problema na hora de enviar a edição definitiva ....

--- TrabalhoPOO1/TrabalhoPOO1/src/App.java
+++
@@ -1,8 +0,0 @@
-import visao.VeiculoMenu;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-    VeiculoMenu menu = new VeiculoMenu();
-    menu.ExibirMenu();
-    }
-}
--- TrabalhoPOO1/TrabalhoPOO1/src/visao/BaseVisao.java
+++
@@ -1,8 +0,0 @@
-package visao;
-
-public abstract class BaseVisao {
-    public BaseVisao(){
-        super();
-    }
-    public abstract void Exibir();
-}
